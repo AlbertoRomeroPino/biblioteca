edd33384617fee344d6a99d1ee46b9d7ed7938ba
elrisaslol <elrisaslol@gmail.com>
2024-12-01 15:09:48 +0100

    primer popup del crud up & running ahora a hacer cntrl c ctrl v y poner todos los popups siuuu,

--- src/main/java/org/chatta/controllers_and_view/PantalladelaBasededatos.java
+++ src/main/java/org/chatta/controllers_and_view/PantalladelaBasededatos.java
@@ -1,6 +1,11 @@
 package org.chatta.controllers_and_view;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 import org.chatta.App;
 
 import java.io.IOException;
@@ -12,7 +17,6 @@ public class PantalladelaBasededatos {
         // Cambiar la vista a la pantalla de Inicio
         App.setRoot(scenes.PANTALLADEINICIO);
     }
-
     @FXML
     private void SwitchToPantalladePrestamos() throws IOException {
         // Cambiar la vista a la pantalla de Inicio
@@ -42,5 +46,36 @@ public class PantalladelaBasededatos {
     private void SwitchToPantalladeEditorial() throws IOException {
         // Cambiar la vista a la pantalla de Inicio
         App.setRoot(scenes.PANTALLADEBASADEDATOSEDITORIAL);
+    }
+
+    @FXML
+    private void CreateNewPrestamo() throws IOException {
+        try {
+            // Cargar el FXML de PantalladeIdentificacion
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/org/chatta/controllers_and_view/createprestamo.fxml"));
+            Parent root = loader.load();
+
+            // Crear un nuevo Stage (ventana emergente)
+            Stage popupStage = new Stage();
+            popupStage.setTitle("Pantalla de Creación de Prestamo");
+            popupStage.initModality(Modality.APPLICATION_MODAL); // Hace que el popup sea modal
+            popupStage.setScene(new Scene(root));
+
+            // Configurar dimensiones predeterminadas
+            popupStage.setWidth(300);  // Ancho de la ventana
+            popupStage.setHeight(350); // Alto de la ventana
+
+            // Opcional: Configurar tamaño mínimo y máximo
+            popupStage.setMinWidth(300);
+            popupStage.setMinHeight(350);
+            popupStage.setMaxWidth(300);
+            popupStage.setMaxHeight(350);
+
+            // Mostrar el popup
+            popupStage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
