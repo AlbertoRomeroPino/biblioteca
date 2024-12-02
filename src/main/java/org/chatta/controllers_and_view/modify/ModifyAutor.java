d4713431210530c578f2c9a3118667c2fa07b2c4
elrisaslol <elrisaslol@gmail.com>
2024-12-02 14:45:15 +0100

    c au c ed c us

--- src/main/java/org/chatta/controllers_and_view/modify/ModifyAutor.java
+++ src/main/java/org/chatta/controllers_and_view/modify/ModifyAutor.java
@@ -3,13 +3,24 @@ package org.chatta.controllers_and_view.modify;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import model.dao.AutorDAO;
+import model.entity.Autor;
 import org.chatta.App;
 import org.chatta.controllers_and_view.scenes;
 
 import java.io.IOException;
 
 public class ModifyAutor {
+
+    @FXML
+    public DatePicker fecha_nacimiento;
+    @FXML
+    public TextField nombre;
+    @FXML
+    public TextField nacionalidad;
     @FXML
     private Button closeButton;
 
@@ -18,6 +29,14 @@ public class ModifyAutor {
     private void Close() throws IOException {
 
         App.setRoot(scenes.PANTALLADEBASADEDATOSAUTORES);
+
+        Autor autor= new Autor();
+        autor.setNombre(nombre.getText());
+        autor.setNacionalidad(nacionalidad.getText());
+        autor.setFechaNacimiento(fecha_nacimiento.getValue());
+
+        AutorDAO.build().store(autor);
+
         Alert alert=new Alert(Alert.AlertType.INFORMATION);
         alert.setTitle("modificación correcta");
         alert.setHeaderText("la modificación a sido completada con exito");
