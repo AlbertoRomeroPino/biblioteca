57f0086e54981e7f840f7e6a78204025d9e75739
elrisaslol <elrisaslol@gmail.com>
2024-12-05 13:39:27 +0100

    c au c ed c us

--- src/main/java/org/chatta/controllers_and_view/create/CreatePrestamo.java
+++ src/main/java/org/chatta/controllers_and_view/create/CreatePrestamo.java
@@ -1,9 +1,17 @@
 package org.chatta.controllers_and_view.create;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
+import model.dao.PrestamoDAO;
+import model.dao.PublicacionDAO;
+import model.dao.UsuarioDAO;
+import model.entity.Enum.Estado_Enum;
+import model.entity.Prestamo;
+import model.entity.Publicacion;
+import model.entity.Usuario;
 import org.chatta.App;
 import org.chatta.controllers_and_view.scenes;
 
@@ -11,7 +19,35 @@ import java.io.IOException;
 
 public class CreatePrestamo {
     @FXML
+    private ComboBox<Estado_Enum> ComboBoxestadoEnum;
+    @FXML
+    private ComboBox<Publicacion> comboboxPublicacion;
+    @FXML
+    private ComboBox<Usuario> comboboxUsuarios;
+    @FXML
+    public DatePicker fechaPrestamo;
+    @FXML
     private Button closeButton;
+
+    private PublicacionDAO publicacionDAO;
+    private UsuarioDAO userDAO;
+
+    public CreatePrestamo() {
+        // Inicializar el DAO
+
+        userDAO = UsuarioDAO.build();
+        publicacionDAO=PublicacionDAO.build();
+    }
+
+    @FXML
+    public void initialize() {
+        // Cargar los autores en el ComboBox al inicio
+
+        ObservableList<Usuario> usuarios = FXCollections.observableArrayList(userDAO.findAll());
+        comboboxUsuarios.setItems(usuarios);
+        ObservableList<Publicacion> publicaciones = FXCollections.observableArrayList(publicacionDAO.findAll());
+        comboboxPublicacion.setItems(publicaciones);
+    }
 
     private void showAlert(Alert.AlertType type, String title, String header, String content) {
         Alert alert = new Alert(type);
@@ -19,6 +55,43 @@ public class CreatePrestamo {
         alert.setHeaderText(header);
         alert.setContentText(content);
         alert.showAndWait();
+    }
+    @FXML
+    private void Close() throws IOException {
+
+        Publicacion publicacionSeleccionado = comboboxPublicacion.getSelectionModel().getSelectedItem();
+        Usuario usuarioSeleccionado = comboboxUsuarios.getSelectionModel().getSelectedItem();
+
+        if (publicacionSeleccionado == null || usuarioSeleccionado == null) {
+            showAlert(Alert.AlertType.ERROR, "Inserción Incorrecta", "La inserción no ha sido completada con éxito",
+                    "Por favor, selecciona un autor y un libro.");
+            return;
+        }
+        if (fechaPrestamo.getValue() == null) {
+            showAlert(Alert.AlertType.ERROR, "Inserción Incorrecta", "La inserción no ha sido completada con éxito",
+                    "El campo 'fecha de Prestamo' está vacío. Por favor, ingréselo.");
+            return;
+        }
+        // Si todos los campos están correctamente completados
+
+        Prestamo prestamo = new Prestamo();
+        prestamo.setPublicacion(publicacionSeleccionado);
+        prestamo.setUsuario(usuarioSeleccionado);
+        prestamo.setFechaPrestamo(fechaPrestamo.getValue());
+        prestamo.setFechaDevolucion((fechaPrestamo.getValue().plusDays(30)));
+        prestamo.setEstado(ComboBoxestadoEnum.getValue());
+
+        PrestamoDAO.build().store(prestamo);
+
+        showAlert(Alert.AlertType.INFORMATION, "Inserción Correcta", "La inserción se ha completado con éxito",
+                "El libro ha sido registrado correctamente.");
+
+        // Cambiar a la pantalla de autores después de insertar el autor
+        App.setRoot(scenes.PANTALLADEBASADEDATOSLIBRO);
+
+        // Cerrar la ventana
+        Stage stage = (Stage) closeButton.getScene().getWindow();
+        stage.close();
     }
 
 
